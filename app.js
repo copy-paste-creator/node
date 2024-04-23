c9e04544014d74908897b5aefe3b4416f928118f
copy-paste-creator <largo-scoring0g@icloud.com>
2024-04-23 22:50:25 +0530

    global objects

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 function sayHello(name) {
   console.log("Hello" + name);
 }
-sayHello("SKC")
+sayHello("SKC");
+
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+var message = " "; // not added to global
+global.console.log();
+global.setTimeout();
+
+// generally variables & functions are added to global scope 'window'
